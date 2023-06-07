d7f7c44b71a3adc42f13ddba4e150c9c66095840
Muhammad Rizky Firdaus <rizkyfirdaus567@gmail.com>
2023-06-08 02:56:49 +0700

    up

--- routes/product.js
+++ routes/product.js
@@ -50,7 +50,7 @@ router.get('/others', async (req, res) => {
     })
 })
 
-router.get('/snacks', async (req, res) => {
+router.get('/snack', async (req, res) => {
     connection.query('SELECT * FROM product WHERE category_prod = "Snaks"', function (err, rows) {
         if (err) {
             return res.status(500).json({
@@ -63,6 +63,19 @@ router.get('/snacks', async (req, res) => {
                 dataProd: rows
             })
         }
+    })
+})
+
+router.post('/order', async (req, res) => {
+    const { userID, prodID, jmlh, total } = req.body
+
+    //const encrytedPassword = await bcrypt.hash(pass, 10) //di encrypt. 10 umunya seperti itu di dokumen
+    connection.query(`INSERT INTO pesanan_user (user_ID,product_id,jmlh_pesanan,total_price)  VALUES (${userID}, ${prodID},${jmlh}, "${total}")`, (err, result) => {
+        if (err) throw err
+        res.status(200).json({
+            registered: result,
+            metadata: "register success"
+        })
     })
 })
 
